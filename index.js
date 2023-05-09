fccd1fcaadbc9304cdd8f33e94ecdb98f4e36d8b
rohit8686 <88605957+rohit8686@users.noreply.github.com>
2023-05-09 10:07:55 +0530

    styling and minor issue fixes

--- index.js
+++ index.js
@@ -19,20 +19,18 @@ const getLifts = () => {
     </div>`;
   }
   return liftsHtml;
-  // return `${Array(noOfLifts).map((_, index) => {
-  //   return `Lift ${index}`;
-  // })}</div>`;
 };
 
 const getNearestLift = (floorIndex) => {
   let comingFloorIndexArr = [];
   let comingFloorIndex;
 
-  // console.log("lifts data ", liftsData, "floor idx ", floorIndex);
-
   // checking lifts below current floor
   for (let i = floorIndex - 1; i >= 0; i--) {
-    if (liftsData[i].hasLift && !liftsData[i].isBusy) {
+    if (
+      liftsData[i].hasLift &&
+      (!liftsData[i].isBusy || liftsData[i].liftsArr.length > 1)
+    ) {
       comingFloorIndexArr.push(i);
       break;
     }
@@ -40,13 +38,14 @@ const getNearestLift = (floorIndex) => {
 
   // checking lifts above current floor
   for (let i = floorIndex + 1; i <= noOfFloors - 1; i++) {
-    // debugger;
-    if (liftsData[i].hasLift && !liftsData[i].isBusy) {
+    if (
+      liftsData[i].hasLift &&
+      (!liftsData[i].isBusy || liftsData[i].liftsArr.length > 1)
+    ) {
       comingFloorIndexArr.push(i);
       break;
     }
   }
-  //   console.log("coming floor index arr", comingFloorIndexArr);
 
   if (comingFloorIndexArr.length === 2) {
     if (
@@ -57,15 +56,10 @@ const getNearestLift = (floorIndex) => {
     } else {
       comingFloorIndex = comingFloorIndexArr[1];
     }
-    // comingFloorIndex = Math.min(
-    //   floorIndex - comingFloorIndexArr[0],
-    //   comingFloorIndexArr[1] - floorIndex
-    // );
   } else if (comingFloorIndexArr.length === 1) {
     comingFloorIndex = comingFloorIndexArr[0];
   }
   const { liftsArr, busyLiftIndex } = liftsData[comingFloorIndex] || {};
-  //   console.log("coming floor index", comingFloorIndex);
 
   return {
     comingFloorIndex,
@@ -74,13 +68,9 @@ const getNearestLift = (floorIndex) => {
 };
 
 const handleLifts = (floorIndex) => {
-  //   console.log("-------------------------------");
   let liftIndex;
   let comingFloorIndex;
-  if (
-    liftsData[floorIndex].hasLift
-    // && !liftsData[floorIndex].isBusy
-  ) {
+  if (liftsData[floorIndex].hasLift) {
     liftIndex = liftsData[floorIndex].liftsArr[0];
     comingFloorIndex = floorIndex;
   } else {
@@ -91,49 +81,29 @@ const handleLifts = (floorIndex) => {
   }
 
   const lifts = document.querySelectorAll(".lift");
-  //   console.log(
-  //     "lifts data => ",
-  //     liftsData,
-  //     "floorIndex is => ",
-  //     floorIndex,
-  //     "coming florr index => ",
-  //     comingFloorIndex,
-  //     "lift index => ",
-  //     liftIndex
-  //     // lifts[liftIndex]
-  //   );
 
   const animateDoors = () => {
     const leftDoors = document.querySelectorAll(".door-left");
     const rightDoors = document.querySelectorAll(".door-right");
-    // console.log(liftIndex, "left ", leftDoors, "right ", rightDoors);
-    // console.log("DOORS ANIMATINGGG", floorIndex);
 
     if (!leftDoors[liftIndex].classList.contains("leftDoorAnimate")) {
       leftDoors[liftIndex].classList.add("leftDoorAnimate");
-      // console.log("before timeout floorIndex ", floorIndex);
 
       setTimeout(() => {
         leftDoors[liftIndex].classList.remove("leftDoorAnimate");
-        // console.log("after timeout floorIndex ", floorIndex);
-        // console.log("NOT BUSY", floorIndex);
         lifts[liftIndex].removeEventListener("transitionend", animateDoors);
         liftsData[floorIndex].isBusy = false;
         liftsData[floorIndex].busyLiftIndex = "";
-        // console.log("lifts Data final ", liftsData);
       }, 2500);
     }
     if (!rightDoors[liftIndex].classList.contains("rightDoorAnimate")) {
       rightDoors[liftIndex].classList.add("rightDoorAnimate");
       setTimeout(() => {
         rightDoors[liftIndex].classList.remove("rightDoorAnimate");
-        // liftsData[floorIndex].isBusy = false;
-        // liftsData[floorIndex].busyLiftIndex = "";
       }, 2500);
     }
   };
 
-  // const comingLiftIndex = liftsData[liftIndex].liftsArr[0];
   const { liftsArr, busyLiftIndex } = liftsData[comingFloorIndex] || {};
 
   const nonBusyLiftIndex = liftsArr.find(
@@ -156,25 +126,23 @@ const handleLifts = (floorIndex) => {
       liftsData[comingFloorIndex].liftsArr.length > 0 ? true : false;
   }
 
-  // const liftIndex = liftsData[liftIndex].liftsArr[0];
   if (comingFloorIndex !== floorIndex) {
-    lifts[liftIndex].style.transform = `translateY(-${floorIndex * 100}px)`;
+    lifts[liftIndex].style.transform = `translateY(-${
+      floorIndex * 120 + floorIndex - 1
+    }px)`;
     lifts[liftIndex].style["transition-duration"] = `2s`;
-    // console.log("BEFORE TRANSITION END FLOOR INDEX", floorIndex);
-
-    // console.log("AFTER TRANSITION END FLOOR INDEX", floorIndex);
     lifts[liftIndex].addEventListener("transitionend", animateDoors);
-    // animateDoors(liftIndex, floorIndex, lifts)
   } else if (comingFloorIndex === floorIndex) {
+    liftsData[floorIndex].busyLiftIndex = liftIndex;
     if (!liftsData[floorIndex].isBusy) {
       animateDoors();
     }
     liftsData[floorIndex].isBusy = true;
     setTimeout(() => {
       liftsData[floorIndex].isBusy = false;
+      liftsData[floorIndex].busyLiftIndex = "";
     }, 2500);
   }
-  // animateDoors(liftIndex);
 };
 
 const showLiftsAndFloors = () => {
@@ -210,35 +178,38 @@ const showLiftsAndFloors = () => {
   }
 
   const upBtns = document.querySelectorAll(".up");
-  // console.log("lifts dataaaaa is ", liftsData);
-  const down = document.querySelector(".down");
+  const downBtns = document.querySelectorAll(".down");
 
   for (let i = 0; i < upBtns.length; i++) {
-    upBtns[i].addEventListener(
-      "click",
-      () => handleLifts(upBtns.length - 1 - i)
-      // animateDoors(upBtns.length - 1 - i)
+    upBtns[i].addEventListener("click", () =>
+      handleLifts(upBtns.length - 1 - i)
+    );
+    downBtns[i].addEventListener("click", () =>
+      handleLifts(upBtns.length - 1 - i)
     );
   }
-  // up.addEventListener("click", animateDoors);
-  // console.log("lifts & floors ", liftsAndFloors);
 };
 
 const handleError = (e) => {
-  // const { name, value } = e.target;
   const errors = document.querySelectorAll(".error");
-  // const liftErr = document.querySelector(".lift-error");
   let hasError = false;
   e.preventDefault();
   for (let i = 0; i < 2; i++) {
     const { name, value } = e.target[i];
-    if (e.target[i].value <= 0) {
-      hasError = true;
-      errors[i].innerText = `No.of ${name} cannot be less than or equal to 0`;
-      errors[i].classList.remove("hide");
+
+    if (name == "floors") {
+      if (e.target[i].value < 2) {
+        hasError = true;
+        errors[i].innerText = `No.of ${name} must be greater than 1`;
+        errors[i].classList.remove("hide");
+      } else errors[i].classList.add("hide");
     }
-    if (e.target[i].value > 0) {
-      errors[i].classList.add("hide");
+    if (name == "lifts") {
+      if (e.target[i].value < 1) {
+        hasError = true;
+        errors[i].innerText = `No.of ${name} must be greater than 0`;
+        errors[i].classList.remove("hide");
+      } else errors[i].classList.add("hide");
     }
     if (e.target[i].value === "") {
       hasError = true;
@@ -247,30 +218,6 @@ const handleError = (e) => {
     }
   }
   return hasError;
-
-  // if (e.target[0].value <= 0) {
-  //   floorErr.innerText = "No.of floors cannot be less than or equal to 0";
-  //   floorErr.classList.remove("hide");
-  // }
-  // if (e.target[1].value <= 0) {
-  //   liftErr.innerText = "No.of lifts cannot be less than or equal to 0";
-  //   liftErr.classList.remove("hide");
-  // }
-  // if (e.target[0].value > 0) {
-  //   floorErr.classList.add("hide");
-  // }
-  // if (e.target[1].value > 0) {
-  //   liftErr.classList.add("hide");
-  // }
-  // if (e.target[0].value === "") {
-  //   floorErr.innerText = "No.of floors cannot be empty";
-  //   floorErr.classList.remove("hide");
-  // }
-  // if (e.target[1].value === "") {
-  //   liftErr.innerText = "No.of lifts cannot be empty";
-  //   liftErr.classList.remove("hide");
-  //   return;
-  // }
 };
 
 const saveFloorsAndLifts = (e) => {
@@ -281,8 +228,18 @@ const saveFloorsAndLifts = (e) => {
     noOfLifts = e.target[1].value;
     form.style.display = "none";
     liftsAndFloors.classList.remove("hide");
+    backBtn.classList.remove("hide");
     showLiftsAndFloors();
   }
 };
 
+const handleBack = () => {
+  form.style.display = "flex";
+  liftsAndFloors.classList.add("hide");
+  backBtn.classList.add("hide");
+  floors.value = "";
+  lifts.value = "";
+  liftsAndFloors.innerHTML = "";
+};
+backBtn.addEventListener("click", handleBack);
 form.addEventListener("submit", saveFloorsAndLifts);
